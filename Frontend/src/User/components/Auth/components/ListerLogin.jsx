66cce1b2a33f728c26a5fdd43286013043b4d713
Alok Sharma <aloksharma10969@gmail.com>
2023-03-22 21:54:01 +0530

    loginSignupChange

--- Frontend/src/User/components/Auth/components/ListerLogin.jsx
+++ Frontend/src/User/components/Auth/components/ListerLogin.jsx
@@ -133,7 +133,7 @@ function ListerLogin({ login }) {
           type="submit"
           className="w-full mt-3  uppercase text-black bg-blue-100 hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         >
-          Sign Up
+          Login
         </button>
       </form>
     </div>
--- Frontend/src/User/components/Auth/components/UserLogin.jsx
+++ Frontend/src/User/components/Auth/components/UserLogin.jsx
@@ -121,7 +121,7 @@ function UserLogin() {
           type="submit"
           className="w-full mt-3 uppercase text-black bg-blue-100 hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         >
-          Sign Up
+          Login
         </button>
       </form>
     </div>
