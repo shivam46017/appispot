267cf508049d456451978072aa8d33179677647e
Somon Das <somondas067@gmail.com>
2023-04-08 18:50:19 +0530

    updates

--- Frontend/src/User/components/Spot/Product.jsx
+++ Frontend/src/User/components/Spot/Product.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/20/solid";
 import { RadioGroup } from "@headlessui/react";
+import ReactImageMagnify from "react-image-magnify";
+import { DatePicker } from "antd";
+import moment from "moment";
+const { RangePicker } = DatePicker;
+// import "antd/dist/antd.css";
 
 const product = {
   name: "Basic Tee 6-Pack",
@@ -63,6 +68,7 @@ function classNames(...classes) {
 export default function Spot() {
   const [selectedColor, setSelectedColor] = useState(product.colors[0]);
   const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
+  const [dates, setDates] = useState([]);
 
   return (
     <div className="bg-white">
@@ -108,35 +114,92 @@ export default function Spot() {
 
         {/* Image gallery */}
         <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-          <div className="aspect-w-3 aspect-h-4 hidden overflow-hidden rounded-lg lg:block">
-            <img
+          <div className="aspect-w-3 aspect-h-4  overflow-hidden rounded-lg lg:block">
+            {/* <img
               src={product.images[0].src}
               alt={product.images[0].alt}
               className="h-full w-full object-cover object-center"
+            /> */}
+            <ReactImageMagnify
+              {...{
+                smallImage: {
+                  alt: "Wristwatch by Ted Baker London",
+                  isFluidWidth: true,
+                  src: product.images[0].src,
+                },
+                largeImage: {
+                  src: product.images[0].src,
+                  width: 1200,
+                  height: 1800,
+                },
+              }}
             />
           </div>
           <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
             <div className="aspect-w-3 aspect-h-2 overflow-hidden rounded-lg">
-              <img
+              <ReactImageMagnify
+                {...{
+                  smallImage: {
+                    alt: "Wristwatch by Ted Baker London",
+                    isFluidWidth: true,
+                    src: product.images[1].src,
+                  },
+                  largeImage: {
+                    src: product.images[1].src,
+                    width: 1200,
+                    height: 1800,
+                  },
+                }}
+              />
+
+              {/* <img
                 src={product.images[1].src}
                 alt={product.images[1].alt}
                 className="h-full w-full object-cover object-center"
-              />
+              /> */}
             </div>
             <div className="aspect-w-3 aspect-h-2 overflow-hidden rounded-lg">
-              <img
+              <ReactImageMagnify
+                {...{
+                  smallImage: {
+                    alt: "Wristwatch by Ted Baker London",
+                    isFluidWidth: true,
+                    src: product.images[2].src,
+                  },
+                  largeImage: {
+                    src: product.images[2].src,
+                    width: 1200,
+                    height: 1800,
+                  },
+                }}
+              />
+              {/* <img
                 src={product.images[2].src}
                 alt={product.images[2].alt}
                 className="h-full w-full object-cover object-center"
-              />
+              /> */}
             </div>
           </div>
           <div className="aspect-w-4 aspect-h-5 sm:overflow-hidden sm:rounded-lg lg:aspect-w-3 lg:aspect-h-4">
-            <img
+            <ReactImageMagnify
+              {...{
+                smallImage: {
+                  alt: "Wristwatch by Ted Baker London",
+                  isFluidWidth: true,
+                  src: product.images[3].src,
+                },
+                largeImage: {
+                  src: product.images[3].src,
+                  width: 1200,
+                  height: 1800,
+                },
+              }}
+            />
+            {/* <img
               src={product.images[3].src}
               alt={product.images[3].alt}
               className="h-full w-full object-cover object-center"
-            />
+            /> */}
           </div>
         </div>
 
@@ -144,14 +207,17 @@ export default function Spot() {
         <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
           <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
             <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-              {product.name}
+              Name of Location
+            </h1>
+            <h1 className="text-2xl font-bold tracking-tight mt-3 text-gray-900 sm:text-2xl">
+              Name of Spot
             </h1>
           </div>
 
           {/* Options */}
           <div className="mt-4 lg:row-span-3 lg:mt-0">
             <h2 className="sr-only">Product information</h2>
-            <p className="text-3xl tracking-tight text-gray-900">
+            <p className="text-3xl tracking-tight text-blue-500">
               {product.price}
             </p>
 
@@ -165,8 +231,8 @@ export default function Spot() {
                       key={rating}
                       className={classNames(
                         reviews.average > rating
-                          ? "text-gray-900"
-                          : "text-gray-200",
+                          ? "text-blue-600"
+                          : "text-blue-300",
                         "h-5 w-5 flex-shrink-0"
                       )}
                       aria-hidden="true"
@@ -178,7 +244,7 @@ export default function Spot() {
                   href={reviews.href}
                   className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500"
                 >
-                  {reviews.totalCount} reviews
+                  {/* {reviews.totalCount} reviews */}
                 </a>
               </div>
             </div>
@@ -186,135 +252,30 @@ export default function Spot() {
             <form className="mt-10">
               {/* Colors */}
               <div>
-                <h3 className="text-sm font-medium text-gray-900">Color</h3>
-
-                <RadioGroup
-                  value={selectedColor}
-                  onChange={setSelectedColor}
-                  className="mt-4"
-                >
-                  <RadioGroup.Label className="sr-only">
-                    {" "}
-                    Choose a color{" "}
-                  </RadioGroup.Label>
-                  <div className="flex items-center space-x-3">
-                    {product.colors.map((color) => (
-                      <RadioGroup.Option
-                        key={color.name}
-                        value={color}
-                        className={({ active, checked }) =>
-                          classNames(
-                            color.selectedClass,
-                            active && checked ? "ring ring-offset-1" : "",
-                            !active && checked ? "ring-2" : "",
-                            "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none"
-                          )
-                        }
-                      >
-                        <RadioGroup.Label as="span" className="sr-only">
-                          {" "}
-                          {color.name}{" "}
-                        </RadioGroup.Label>
-                        <span
-                          aria-hidden="true"
-                          className={classNames(
-                            color.class,
-                            "h-8 w-8 rounded-full border border-black border-opacity-10"
-                          )}
-                        />
-                      </RadioGroup.Option>
-                    ))}
-                  </div>
-                </RadioGroup>
+                <h3 className="text-sm font-medium text-gray-900">Date</h3>
+                {/* //>>Put "Set data here" */}
+                <RangePicker
+                onChange={(values)=>{
+                  setDates(values.map((item)=>{
+                    return moment(item).format("DD-MM-YYYY")
+                  }))
+                }}
+                 className="p-3 mt-3 text-lg"
+                 />
               </div>
 
               {/* Sizes */}
-              <div className="mt-10">
+              {/* <div className="mt-10">
                 <div className="flex items-center justify-between">
                   <h3 className="text-sm font-medium text-gray-900">Size</h3>
-                  <a
-                    href="#"
-                    className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    Size guide
-                  </a>
                 </div>
-
-                <RadioGroup
-                  value={selectedSize}
-                  onChange={setSelectedSize}
-                  className="mt-4"
-                >
-                  <RadioGroup.Label className="sr-only">
-                    {" "}
-                    Choose a size{" "}
-                  </RadioGroup.Label>
-                  <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
-                    {product.sizes.map((size) => (
-                      <RadioGroup.Option
-                        key={size.name}
-                        value={size}
-                        disabled={!size.inStock}
-                        className={({ active }) =>
-                          classNames(
-                            size.inStock
-                              ? "cursor-pointer bg-white text-gray-900 shadow-sm"
-                              : "cursor-not-allowed bg-gray-50 text-gray-200",
-                            active ? "ring-2 ring-indigo-500" : "",
-                            "group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6"
-                          )
-                        }
-                      >
-                        {({ active, checked }) => (
-                          <>
-                            <RadioGroup.Label as="span">
-                              {size.name}
-                            </RadioGroup.Label>
-                            {size.inStock ? (
-                              <span
-                                className={classNames(
-                                  active ? "border" : "border-2",
-                                  checked
-                                    ? "border-indigo-500"
-                                    : "border-transparent",
-                                  "pointer-events-none absolute -inset-px rounded-md"
-                                )}
-                                aria-hidden="true"
-                              />
-                            ) : (
-                              <span
-                                aria-hidden="true"
-                                className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
-                              >
-                                <svg
-                                  className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
-                                  viewBox="0 0 100 100"
-                                  preserveAspectRatio="none"
-                                  stroke="currentColor"
-                                >
-                                  <line
-                                    x1={0}
-                                    y1={100}
-                                    x2={100}
-                                    y2={0}
-                                    vectorEffect="non-scaling-stroke"
-                                  />
-                                </svg>
-                              </span>
-                            )}
-                          </>
-                        )}
-                      </RadioGroup.Option>
-                    ))}
-                  </div>
-                </RadioGroup>
-              </div>
+              </div> */}
 
               <button
                 type="submit"
                 className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
-                Add to bag
+                Book Now
               </button>
             </form>
           </div>
